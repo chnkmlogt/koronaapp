78eb9eb5890d9b8fecdde7f044ebf1e4e1df7750
Cihan Öğüt <cihan.ogut@topkapidanismanlik.com.tr>
2025-06-01 13:18:36 +0300

    CrossOrigin

--- backend/src/main/java/com/chnkmlogt/korona/controller/CityController.java
+++ backend/src/main/java/com/chnkmlogt/korona/controller/CityController.java
@@ -11,7 +11,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/api")
-@CrossOrigin
+@CrossOrigin(origins = "*")
 public class CityController {
     @Autowired
     private CityService cityService;
--- backend/src/main/java/com/chnkmlogt/korona/controller/ParsedNewsController.java
+++ backend/src/main/java/com/chnkmlogt/korona/controller/ParsedNewsController.java
@@ -16,7 +16,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/api")
-@CrossOrigin
+@CrossOrigin(origins = "*")
 public class ParsedNewsController {
     private final ParsedNewsService newsService;
 
